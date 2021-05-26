0451f030dff2b42621ca2cad4179fdf1ad15b69a
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-05-26 09:53:51 +0200

    dbcontext och seed metod

---
+++ Partners_In_Crime/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Partners_In_Crime.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        private static void Seed()
+        {
+
+        }
+    }
+}
