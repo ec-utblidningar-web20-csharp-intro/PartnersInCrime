7d89f95974edfe016fc28d55d6d33813578e1b7d
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-05-26 10:02:53 +0200

    dbsets

--- Partners_In_Crime/Data/ApplicationDbContext.cs
+++ Partners_In_Crime/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Partners_In_Crime.Models;
 
 namespace Partners_In_Crime.Data
 {
@@ -12,7 +13,10 @@ namespace Partners_In_Crime.Data
             : base(options)
         {
         }
-
+        
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Hobbies> Hobbies { get; set; }
+        public DbSet<Interests> Interests { get; set; }
 
         private static void Seed()
         {
