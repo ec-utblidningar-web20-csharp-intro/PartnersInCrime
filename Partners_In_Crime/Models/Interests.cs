e7ebedd50cf12b05cbb662abe32f667944781a04
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-05-26 09:53:31 +0200

    properties

---
+++ Partners_In_Crime/Models/Interests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partners_In_Crime.Models
+{
+    public class Interests
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
