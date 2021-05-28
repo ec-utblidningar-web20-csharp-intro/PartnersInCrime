8fbc06c912b4271663372709ca910931b3647d7d
Jack Gelotte <jackgelotte@gmail.com>
2021-05-28 15:11:58 +0200

    Ingenting direkt...

---
+++ Partners_In_Crime/Controllers/MatchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partners_In_Crime.Controllers
+{
+    public class MatchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Här är det tomt Bellz
+    }
+}
