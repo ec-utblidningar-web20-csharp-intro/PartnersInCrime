9c99da0d25acba664894efa28d7b30d497f0a923
Jack Gelotte <jackgelotte@gmail.com>
2021-05-31 13:30:37 +0200

    kopplade m med v med c

--- Partners_In_Crime/Controllers/MatchController.cs
+++ Partners_In_Crime/Controllers/MatchController.cs
@@ -19,22 +19,24 @@ namespace Partners_In_Crime.Controllers
 
         public IActionResult Index()
         {
-            var allUsers = _context.AppUsers.Include(e=>e.Interests);
-            var currentUserInterests = allUsers.First();
-            Match(currentUserInterests, allUsers.Skip(1), 5, MatchOptions.Both);
-            return View();
+            var allUsers = _context.AppUsers.Include(e=>e.Interests).Include(e=>e.Hobbies);
+            var currentUser = allUsers.First();
+            var generalMatches = Match(currentUser, allUsers.Skip(1), 5, MatchOptions.Both);
+            var viewModel = new MatchViewModel(currentUser, generalMatches);
+        
+            return View(viewModel);
         }
 
         // Matches current user with other users on interests, hobbies or both. Default: Interests.
-        public IEnumerable<AppUser> Match(AppUser currentUser, IEnumerable<AppUser> allUsers, int returnCount, MatchOptions matchOptions = MatchOptions.Interests)
+        public IEnumerable<IGrouping<int, AppUser>> Match(AppUser currentUser, IEnumerable<AppUser> allUsers, int returnCount, MatchOptions matchOptions = MatchOptions.Interests)
         {
             if(matchOptions == MatchOptions.Interests)
-                return allUsers.GroupBy(e => e.Interests.Where(m => currentUser.Interests.Contains(m)).Count()).OrderByDescending(g => g.Key).SelectMany(u => u).Take(returnCount);
+                return allUsers.GroupBy(e => e.Interests.Where(m => currentUser.Interests.Contains(m)).Count()).OrderByDescending(g => g.Key).Take(returnCount);
 
             if(matchOptions==MatchOptions.Hobbies)
-                return allUsers.GroupBy(e => e.Hobbies.Where(m => currentUser.Hobbies.Contains(m)).Count()).OrderByDescending(g => g.Key).SelectMany(u => u).Take(returnCount);
+                return allUsers.GroupBy(e => e.Hobbies.Where(m => currentUser.Hobbies.Contains(m)).Count()).OrderByDescending(g => g.Key).Take(returnCount);
 
-            return allUsers.GroupBy(e => e.Interests.Where(m => currentUser.Interests.Contains(m)).Count() + e.Hobbies.Where(m => currentUser.Hobbies.Contains(m)).Count()).OrderByDescending(g => g.Key).SelectMany(u => u).Take(returnCount);
+            return allUsers.GroupBy(e => e.Interests.Where(m => currentUser.Interests.Contains(m)).Count() + e.Hobbies.Where(m => currentUser.Hobbies.Contains(m)).Count()).OrderByDescending(g => g.Key).Take(returnCount);
         }
 
         public enum MatchOptions
---
+++ Partners_In_Crime/Models/MatchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partners_In_Crime.Models
+{
+    public class MatchViewModel
+    {
+        public MatchViewModel(AppUser appUser, IEnumerable<IGrouping<int, AppUser>> matchedUsers, Hobby hobby = null, Interest interest = null )
+        {
+            AppUser = appUser;
+            Hobby = hobby;
+            Interest = interest;
+            MatchedUsers = matchedUsers.Where(m=>m.Key != 0);
+            NotMatchedUsers = matchedUsers.Where(m => m.Key == 0);
+        }
+        public AppUser AppUser { get; set; }
+        
+        public Hobby Hobby { get; set; }
+        public Interest Interest { get; set; }
+        public IEnumerable<IGrouping<int, AppUser>> MatchedUsers { get; set; }
+        public IEnumerable<IGrouping<int, AppUser>> NotMatchedUsers { get; set; }
+
+    }
+}
