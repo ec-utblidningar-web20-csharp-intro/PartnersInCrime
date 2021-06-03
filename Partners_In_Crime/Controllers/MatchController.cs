3f7e211eca32ac470005b10a2419057e7e79a16c
Isabella Pettersson <isabella.pettersso1@learnet.se>
2021-06-03 10:34:31 +0200

    search result method with a test search

--- Partners_In_Crime/Controllers/MatchController.cs
+++ Partners_In_Crime/Controllers/MatchController.cs
@@ -36,8 +36,11 @@ namespace Partners_In_Crime.Controllers
             var interestMatches = Match(currentUser, allUsers.Skip(1), 5, MatchOptions.Interests);
             viewModel.InterestMatch = new InterestMatchViewModel(currentUser, interestMatches);
 
-            var interests = _context.Interests.Where(i => i.Name == "grg").ToList();
+            // Gör en sökning med IT och Cooking som hobby/interest parametrar
+            var interests = _context.Interests.Where(i => i.Name == "IT").ToList();
             var hobbies = _context.Hobbies.Where(h => h.Name == "Cooking").ToList();
+            var searchResults = GetSearchedUsers(allUsers, interests, hobbies);
+            viewModel.SearchResult = new SearchResultViewModel { Users = searchResults };
 
             return View(viewModel);
         }
@@ -75,53 +78,54 @@ namespace Partners_In_Crime.Controllers
             return new List<AppUser>();
         }
 
-        public List<AppUser> FindUsers(List<Hobby> hobbies = null, List<Interest> interests = null)
-        {
-            var result = new List<AppUser>();
+        // Alternativ GetSearchedUsers metod
+        //public List<AppUser> FindUsers(List<Hobby> hobbies = null, List<Interest> interests = null)
+        //{
+        //    var result = new List<AppUser>();
 
-            var users = _context.AppUsers.Include(a => a.Hobbies).Include(a => a.Interests).ToList();
+        //    var users = _context.AppUsers.Include(a => a.Hobbies).Include(a => a.Interests).ToList();
 
-            var noMatchHobbies = new List<Hobby>();
-            var matchedHobbies = new List<Hobby>();
+        //    var noMatchHobbies = new List<Hobby>();
+        //    var matchedHobbies = new List<Hobby>();
 
-            var noMatchInterests = new List<Interest>();
-            var matchedInterests = new List<Interest>();
+        //    var noMatchInterests = new List<Interest>();
+        //    var matchedInterests = new List<Interest>();
 
-            foreach (var user in users)
-            {
-                if (hobbies != null)
-                {
-                    noMatchHobbies = user.Hobbies.Except(hobbies).ToList();
-                    matchedHobbies = user.Hobbies.Except(noMatchHobbies).ToList();
+        //    foreach (var user in users)
+        //    {
+        //        if (hobbies != null)
+        //        {
+        //            noMatchHobbies = user.Hobbies.Except(hobbies).ToList();
+        //            matchedHobbies = user.Hobbies.Except(noMatchHobbies).ToList();
 
-                    if (interests != null)
-                    {
-                        noMatchInterests = user.Interests.Except(interests).ToList();
-                        matchedInterests = user.Interests.Except(noMatchInterests).ToList();
+        //            if (interests != null)
+        //            {
+        //                noMatchInterests = user.Interests.Except(interests).ToList();
+        //                matchedInterests = user.Interests.Except(noMatchInterests).ToList();
 
-                        if (matchedHobbies.Count == hobbies.Count && matchedInterests.Count == interests.Count)
-                        {
-                            result.Add(user);
-                        }
-                    }
-                }
-                if (interests != null)
-                {
-                    noMatchInterests = user.Interests.Except(interests).ToList();
-                    matchedInterests = user.Interests.Except(noMatchInterests).ToList();
+        //                if (matchedHobbies.Count == hobbies.Count && matchedInterests.Count == interests.Count)
+        //                {
+        //                    result.Add(user);
+        //                }
+        //            }
+        //        }
+        //        if (interests != null)
+        //        {
+        //            noMatchInterests = user.Interests.Except(interests).ToList();
+        //            matchedInterests = user.Interests.Except(noMatchInterests).ToList();
 
-                    if (matchedInterests.Count == interests.Count)
-                    {
-                        result.Add(user);
-                    }
-                }
-                if (matchedInterests.Count == interests.Count)
-                {
-                    result.Add(user);
-                }
-            }
+        //            if (matchedInterests.Count == interests.Count)
+        //            {
+        //                result.Add(user);
+        //            }
+        //        }
+        //        if (matchedInterests.Count == interests.Count)
+        //        {
+        //            result.Add(user);
+        //        }
+        //    }
 
-            return result;
-        }
+        //    return result;
+        //}
     }
 }
